77a0db86aa78a5b94e59c7d2acb5b7a506e766df
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-07 03:43:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function add(a,b){
 function substruct(a,b){
   return a-b
 }
-function add(a,b){
-  return a+b
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(n){
+  return n++
 }
